803778be1c8fee3cf5221f245d798d7297a693e9
John Roche <jroche@redhat.com>
2017-08-04 12:34:38 +0100

    Update config for Openshift

--- config/ose-placeholders.js
+++ config/ose-placeholders.js
@@ -1,8 +1,20 @@
 //conf-docker.json is generated from this in the Dockerfile
 var placeholders = {
-  "port": 8080,
+  "mbaasType": "openshift",
+  "https.enabled": false,
+  "port": "{{env.FH_DATAMAN_PORT}}",
+  "sizeLimit": 0,
   "logger.streams[0].level": "{{env.FH_LOG_LEVEL}}",
-  "logger.streams[1].level": "{{env.FH_LOG_LEVEL}}"
+  "logger.streams[1].level": "{{env.FH_LOG_LEVEL}}",
+  "auth.secret": "{{env.FHMBAAS_KEY}}",
+  "businessObjects": [
+    "cluster/reseller/customer/domain/service/data-browser",
+    "cluster/reseller/customer/domain/project/client-apps/data-browser",
+    "cluster/reseller/customer/domain/project/cloud-apps/data-browser"
+  ],
+  "mbaas.url": "{{env.FHMBAAS_URL}}",
+  "mbaas.password": "{{env.MONGODB_FHMBAAS_PASSWORD}}",
+  "mbaas.username": "{{env.MONGODB_FHMBAAS_USER}}"
 };
 
 module.exports = placeholders;
